6f92043d109bab805af40e6cb1114ece89b1b1f5
José <88600536+Vorztek@users.noreply.github.com>
2021-10-12 09:52:19 +0200

    Add files via upload

---
+++ algo(britto)/Projet_Algo_Prog_VS_Code/Application.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Application
+{
+	public void fonctionPrincipale ()
+	{
+	Console.WriteLine("Choisissez votre burger: Boeuf = 1 , Poulet = 2, Poisson = 3");
+	
+
+	do {
+		int menu =	Utilisateur.saisirEntier(); 
+		}
+		while( menu < 1 & > 3);
+	
+	switch (menu)
+	{
+		case 1:
+		Console.WriteLine("Nous préparons votre burger au boeuf");
+		break;
+		case 2:
+		Console.WriteLine("Nous préparons votre burger au poulet");
+		break;
+		case 3:
+		Console.WriteLine("Nous préparons votre burger poisson");
+		break;
+		default:
+		break;
+	}	
+
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ algo(britto)/Projet_Algo_Prog_VS_Code/application1.cs
@@ -0,0 +1,28 @@
+﻿
+		//DEBUT de votre programme
+		//FIN de votre programme
+	
+
+	//DECLAREZ VOS FONCTIONS EN DESSOUS DE CETTE LIGNE
+
+	/* EXEMPLES DE PROCEDURES ET FONCTIONS :
+	
+		void uneProcedure(int param1, int param2)
+		{
+			Console.WriteLine(param1+param2);
+		{
+		
+		int uneFonction(int param1, int param2)
+		{
+			int resultat = 0;
+			resultat = param1 + param2;
+			return resultat;
+		}
+		
+		
+		*/
+
+	//NE PAS TOUCHER LE CODE EN DESSOUS DE CETTE LIGNE
+
+
+
